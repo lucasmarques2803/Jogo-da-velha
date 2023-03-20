cee455187c608aa237d480022025d4a520be5869
Lucas Marques <lucas-marques2003@hotmail.com>
2023-03-20 08:55:33 -0300

    Criado o menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Sobre from './objects/Sobre';
+import Menu from './objects/Menu';
 
 function App() {
   return (
-    <Sobre />
+    <>
+      <Sobre />
+      <Menu />
+    </>
   );
 }
 
